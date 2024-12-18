cb7307eb7516fdf8923e5102d26661c9fad3de80
Brandon Gachenot <brandon@gachenot.eu>
2024-12-18 18:24:03 +0100

    ci: add integration self hosted

--- tests/utils/render.sh
+++ tests/utils/render.sh
@@ -9,7 +9,16 @@ set -u
 function main()
 {
   readonly template="$1"; shift
-  readonly content="$(cat "$template")"
+
+  # Check if at least one of the variables is set
+  if [[ -z "${MANAGEMENT_API_TOKEN:-}" && -z "${HEALTHCHECKSIO_API_TOKEN:-}" ]]; then
+    echo "Error: Either MANAGEMENT_API_TOKEN or HEALTHCHECKSIO_API_TOKEN must be set."
+    exit 1
+  fi
+
+  local content
+  content="$(cat "$template")"
+  readonly content
 
   eval "echo \"$content\""
 }
